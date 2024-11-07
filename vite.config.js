787cb78eff7632f45540647045c6420224f295f6
anshsehgal13 <anshsehgal03@gmail.com>
2024-11-08 02:53:46 +0530

    Final 2

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [React()],
+  plugins: [react()],
   server: {
     port: process.env.PORT || 3000,
     host: '0.0.0.0'
